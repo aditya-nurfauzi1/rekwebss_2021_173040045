162da78f8baa6309d7fc5f351f5840b240000280
aditya <aditya.muh1907@gmail.com>
2021-07-18 23:27:53 +0700

    menambahkan model dan memindahkan data ke model dari routes

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -31,53 +34,17 @@ Route::get('/about', function () {
 
 
 Route::get('/blog', function () {
-    $blog_posts = [
-        [
-        "title" => "Judul Post Pertama",
-        "slug" => "Judul-post-pertama",
-        "author" => "Aditya M Nurfauzi",
-        "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni laborum doloribus illum iure minima quam, odio corrupti cum fugit quo hic facere magnam amet vitae odit impedit consectetur eum iusto.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa enim nesciunt beatae dolorum voluptates ex eos, maxime officia, vel esse nihil sit provident porro eius. Sed quis nulla earum quidem quaerat perspiciatis ut iste. Magnam ipsam a eveniet iusto. Quos asperiores sint aspernatur eaque, eius temporibus cupiditate? Repellat quas minima, soluta labore natus, est itaque esse aperiam pariatur doloribus non ad placeat beatae qui mollitia atque ipsam veniam et saepe, quae doloremque delectus odio impedit excepturi! Perspiciatis suscipit aspernatur aliquid odio voluptate rerum praesentium consequuntur assumenda perferendis delectus, necessitatibus voluptates, quo culpa iusto iste voluptatibus inventore ullam fugit mollitia. Debitis!"
-        ],
-        [
-            "title" => "Judul Post kedua",
-            "slug" => "Judul-post-kedua",
-            "author" => "Ucup",
-            "body" => "Lorem, ipsusectetur adipisicing elit. Magni laborum doloribus illum iure minima quam, odio corrupti cum fugit quo hic facere magnam amet vitae odit impedit consectetur eum iusto.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab modi voluptate similique iste. Sed rem est distinctio ipsum explicabo totam minus temporibus dolor laborum. Cumque, voluptas? Ullam tempore, voluptatem voluptatibus exercitationem asperiores incidunt. Quae, fugiat nihil veritatis tempore exercitationem perspiciatis sapiente impedit ad asperiores laboriosam cum rem, quibusdam libero officia."
-        ]
-    ];
-
-    return view('posts',[
+        return view('posts',[
         "title" => "Blog",
-        "posts" => $blog_posts
+        "posts" => Post::all()
     ]);
 });
 
 
 //halaman single 
 Route::get('posts/{slug}', function($slug) {
-    $blog_posts = [
-        [
-        "title" => "Judul Post Pertama",
-        "slug" => "Judul-post-pertama",
-        "author" => "Aditya M Nurfauzi",
-        "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni laborum doloribus illum iure minima quam, odio corrupti cum fugit quo hic facere magnam amet vitae odit impedit consectetur eum iusto.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa enim nesciunt beatae dolorum voluptates ex eos, maxime officia, vel esse nihil sit provident porro eius. Sed quis nulla earum quidem quaerat perspiciatis ut iste. Magnam ipsam a eveniet iusto. Quos asperiores sint aspernatur eaque, eius temporibus cupiditate? Repellat quas minima, soluta labore natus, est itaque esse aperiam pariatur doloribus non ad placeat beatae qui mollitia atque ipsam veniam et saepe, quae doloremque delectus odio impedit excepturi! Perspiciatis suscipit aspernatur aliquid odio voluptate rerum praesentium consequuntur assumenda perferendis delectus, necessitatibus voluptates, quo culpa iusto iste voluptatibus inventore ullam fugit mollitia. Debitis!"
-        ],
-        [
-            "title" => "Judul Post kedua",
-            "slug" => "Judul-post-kedua",
-            "author" => "Ucup",
-            "body" => "Lorem, ipsusectetur adipisicing elit. Magni laborum doloribus illum iure minima quam, odio corrupti cum fugit quo hic facere magnam amet vitae odit impedit consectetur eum iusto.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab modi voluptate similique iste. Sed rem est distinctio ipsum explicabo totam minus temporibus dolor laborum. Cumque, voluptas? Ullam tempore, voluptatem voluptatibus exercitationem asperiores incidunt. Quae, fugiat nihil veritatis tempore exercitationem perspiciatis sapiente impedit ad asperiores laboriosam cum rem, quibusdam libero officia."
-        ]
-    ];
-
-    $new_post = [];
-    foreach($blog_posts as $post){
-        if($post["slug"] === $slug){
-            $new_post = $post;
-        }
-    }
     return view('post', [
         "title" => "Single Post",
-        "post" => $new_post
+        "post" => Post::find($slug)
     ]);
 });
