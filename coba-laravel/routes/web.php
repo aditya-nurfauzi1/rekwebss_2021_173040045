66674196198736bbba0708fcfe35ac00ad13da9b
aditya <aditya.muh1907@gmail.com>
2021-07-27 03:06:42 +0700

    menyelesaikan video 8

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -39,6 +39,14 @@ Route::get('/blog', [PostController::class, 'index']);
 //halaman single 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
+
+Route::get('/categories', function(){
+    return view('categories', [
+        'title' => 'Post Categories',
+        'categories' => Category::all()
+    ]);
+});
+
 Route::get('/categories/{category:slug}', function(Category $category){
     return view('category', [
         'title' => $category->name,
