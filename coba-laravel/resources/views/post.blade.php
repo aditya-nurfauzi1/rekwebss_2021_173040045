30df24a7cc4e1f43578b2369ab5f9aec344709ad
aditya <aditya.muh1907@gmail.com>
2021-07-27 02:56:13 +0700

    implementasi relationship

--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -2,9 +2,8 @@
 @extends('layouts.main')
 
 @section('container')
-    <article>
-        <h2>{{ $post->title}}</h2>
+        <h2>{{ $post->title}}</h2>    
+        <p>By Aditya in <a href="/categories/{{  $post->category->slug }}">{{  $post->category->name }}</a></p>
         {!! $post->body !!}
-    </article>
-    <a href="/blog">Kembali ke blog</a>
+        <a href="/blog">Kembali ke blog</a>
 @endsection
